de9bb134f024864b5cc71c345377baefd012d834
mario5698 <42937972+mario5698@users.noreply.github.com>
2026-01-01 21:26:02 +0100

    ec2 ssh open

--- main.tf
+++ main.tf
@@ -1,36 +1,64 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+terraform{
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
     }
-  }
 }
 
 provider "aws" {
-  region = "eu-west-1"
+    region = "eu-west-1"
 }
 
 data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
+    most_recent = true
+    filter {
+      name="name"
+      values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    }
   owners = ["099720109477"] # Canonical
 
 }
 
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
 
-  tags = {
-    name = "learn-terraform"
-  }
-
+data "aws_vpc" "default" {
+    default = true
+  
 }
 
+resource "aws_instance" "app_server" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    tags = {
+      name = "learn-terraform"
+    }
+}
+
+resource "aws_security_group" "allow_ssh" {
+    name = "allow_ssh"
+    description = "Allow ssh inbound traffic"
+    vpc_id = data.aws_vpc.default.id
+    tags = {
+      Name = "allow_ssh"
+    }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+    security_group_id = aws_security_group.allow_ssh.id
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr_ipv4 =  "0.0.0.0/0" 
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+    security_group_id = aws_security_group.allow_ssh.id
+    from_port = 0
+    to_port = 0
+    ip_protocol = "-1"
+    cidr_ipv4 =  "0.0.0.0/0" 
+}
 
 
