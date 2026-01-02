05399ef30f5a181aa1c626ae958110c98ef85d08
mario5698 <42937972+mario5698@users.noreply.github.com>
2026-01-02 19:17:18 +0100

    update main

--- main.tf
+++ main.tf
@@ -7,79 +7,10 @@ terraform{
     }
 }
 
-provider "aws" {
-    region = "eu-west-1"
-}
-
-data "aws_ami" "ubuntu" {
-    most_recent = true
-    filter {
-      name="name"
-      values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-    }
-  owners = ["099720109477"] # Canonical
-}
-
 data "aws_vpc" "default" {
     default = true
   
 }
 
-resource "aws_instance" "app_server" {
-    ami = data.aws_ami.ubuntu.id
-    instance_type = var.instance_type
-    key_name = aws_key_pair.mi_key.key_name
-    vpc_security_group_ids = [aws_security_group.allow_ssh.id,aws_security_group.allow_web.id]   
-    tags = {
-      Name = "learn-terraform"
-    }
-    user_data = file("start.sh")
-}
 
-resource "aws_security_group" "allow_ssh" {
-    name = "allow_ssh"
-    description = "Allow ssh inbound traffic"
-    vpc_id = data.aws_vpc.default.id
-    tags = {
-      Name = "allow_ssh"
-    }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
-    security_group_id = aws_security_group.allow_ssh.id
-    from_port = 22
-    to_port = 22
-    ip_protocol = "tcp"
-    cidr_ipv4 =  "0.0.0.0/0" 
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-    security_group_id = aws_security_group.allow_ssh.id
-    from_port = 0
-    to_port = 0
-    ip_protocol = "-1"
-    cidr_ipv4 =  "0.0.0.0/0" 
-}
-
-resource "aws_security_group" "allow_web" {
-    name = "allow_web"
-    description = "Allow ssh inbound traffic"
-    vpc_id = data.aws_vpc.default.id
-    tags = {
-      Name = "allow_web"
-    }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_web_ipv4" {
-    security_group_id = aws_security_group.allow_web.id
-    from_port = 80
-    to_port = 80
-    ip_protocol = "tcp"
-    cidr_ipv4 =  "0.0.0.0/0" 
-}
-
-resource "aws_key_pair" "mi_key" {
-    key_name = "terraform_ec2"
-    public_key = file("~/.ssh/terraform_ec2.pub")
-}
 
