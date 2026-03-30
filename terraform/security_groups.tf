9c8f6d444b006c6ec1cdbebe8c707889ed6e50c4
mario5698 <42937972+mario5698@users.noreply.github.com>
2026-03-30 17:42:17 +0200

    update terrafor rules aws

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -2,70 +2,51 @@ resource "aws_instance" "app_server" {
     ami = data.aws_ami.ubuntu.id
     instance_type = var.instance_type
     key_name = aws_key_pair.mi_key.key_name
-    vpc_security_group_ids = [aws_security_group.allow_ssh.id,aws_security_group.allow_web.id,aws_security_group.allow_api.id]   
+    vpc_security_group_ids = [aws_security_group.app_sg.id]
     tags = {
-      Name = "learn-terraform"
+      Name = "gym_api_server"
     }
 }
 
-resource "aws_security_group" "allow_ssh" {
-    name = "allow_ssh"
-    description = "Allow ssh inbound traffic"
+resource "aws_security_group" "app_sg" {
+    name = "app_sg"
+    description = "security group for gym api "
     vpc_id = data.aws_vpc.default.id
     tags = {
-      Name = "allow_ssh"
+        Name= "app_sg"
     }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
-    security_group_id = aws_security_group.allow_ssh.id
-    from_port = 22
-    to_port = 22
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+    security_group_id = aws_security_group.app_sg.id
     ip_protocol = "tcp"
-    cidr_ipv4 =  "0.0.0.0/0" 
-}
-resource "aws_security_group" "allow_web" {
-    name = "allow_web"
-    description = "Allow ssh inbound traffic"
-    vpc_id = data.aws_vpc.default.id
-    tags = {
-      Name = "allow_web"
-    }
+    from_port = 22
+    to_port = 22    
+    cidr_ipv4 = "0.0.0.0/0"
 }
 
-resource "aws_security_group" "allow_api" {
-    name = "allow_api"
-    description = "Allow api inbound traffic"
-    vpc_id = data.aws_vpc.default.id
-    tags = {
-      Name = "allow_api"
-    }
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-    security_group_id = aws_security_group.allow_ssh.id
-    from_port = 0
-    to_port = 0
-    ip_protocol = "-1"
-    cidr_ipv4 =  "0.0.0.0/0" 
-}
-
-
-resource "aws_vpc_security_group_ingress_rule" "allow_web_ipv4" {
-    security_group_id = aws_security_group.allow_web.id
+resource "aws_vpc_security_group_ingress_rule" "http" {
+    security_group_id = aws_security_group.app_sg.id
+    ip_protocol = "tcp"
     from_port = 80
     to_port = 80
-    ip_protocol = "tcp"
-    cidr_ipv4 =  "0.0.0.0/0" 
+    cidr_ipv4 = "0.0.0.0/0"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_api_ipv4" {
-    security_group_id = aws_security_group.allow_api.id
+resource "aws_vpc_security_group_ingress_rule" "api" {
+    security_group_id = aws_security_group.app_sg.id
+    ip_protocol = "tcp"
     from_port = 5000
     to_port = 5000
-    ip_protocol = "tcp"
-    cidr_ipv4 =  "0.0.0.0/0" 
+    cidr_ipv4 = "0.0.0.0/0"
 }
+
+resource "aws_vpc_security_group_egress_rule" "all_out" {
+    security_group_id = aws_security_group.app_sg.id
+    ip_protocol = "-1"
+    cidr_ipv4 = "0.0.0.0/0"
+}
+
 
 resource "aws_key_pair" "mi_key" {
     key_name = "terraform_ec2"
